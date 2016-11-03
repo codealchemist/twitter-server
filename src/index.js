9fafec56dd067dd9e54480730b99908f9e9ac232
Alberto Miranda <codealchemist@gmail.com>
2016-11-03 11:41:57 -0300

    removed errorhandler; updated winston; added mocha to package.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const errorhandler = require('errorhandler')
 const http = require('http')
 const cors = require('cors')
 const app = express()
